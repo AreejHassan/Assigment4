a294d3f2a20c9afcf26269669e7e1798e1d1dd25
AreejHassan <161451070+AreejHassan@users.noreply.github.com>
2024-10-20 19:03:42 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Card from "@/components/card";
+import React from "react"
+export default function Home ()  {
+  return (
+    <>
+    <Card name='Areej' rollno={3377} day='Monday' /><br/>
+    <Card name='Areesha' rollno={7744} day='Tuesday' /><br/>
+    <Card name='Tahreem' rollno={8899} day='Friday' /><br/>
+     </>
+  )}
